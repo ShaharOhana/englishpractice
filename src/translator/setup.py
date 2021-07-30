dd862be4765867fde78f6fe78a84e99cc2e63e84
Shahar Ohana <shaharohana4@gmail.com>
2021-07-30 19:02:41 +0300

    add basic package structure

---
+++ src/translator/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='translator',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Small game to practice words to Amiram test',
+    install_requires=[],
+    author='Shahar Ohana',
+    author_email='shaharohana4@gmail.com',
+    python_requires='>3.8'
+)
