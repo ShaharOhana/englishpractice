8e4c3389f3393c67469b740a199d289c928db7a3
Shahar Ohana <shaharohana4@gmail.com>
2021-07-30 20:15:03 +0300

    get game words according to user words' request

--- src/translator/translator/game.py
+++ src/translator/translator/game.py
@@ -1,9 +1,10 @@
-from typing import Tuple
+from random import choice
+from typing import Tuple, Dict
 
 from .words import WORDS
 
 
-def get_game_mode() -> Tuple[int, str]:
+def get_game_mode() -> Tuple[int, int]:
     print('Hello & Welcome to translate words game!')
     print('Choose your game mode:')
     print('1. Practice with hints')
@@ -14,9 +15,35 @@ def get_game_mode() -> Tuple[int, str]:
           'Press Enter for all words, or insert how many words you wish to practice')
     words_count = input('Insert here:')
 
-    return int(user_choice), words_count or str(len(WORDS))
+    return int(user_choice), int(words_count) or len(WORDS)
+
+
+def get_random_words(words_count: int) -> Dict:
+    """
+    Get random chose words from words collection.
+    """
+    words_keys = list(WORDS)
+    game_words = {}
+
+    for _ in range(words_count):
+        word = choice(words_keys)
+        while word in game_words:
+            word = choice(words_keys)
+
+        if word not in game_words:
+            game_words[word] = WORDS[word]
+
+    assert len(game_words) == words_count
+    return game_words
+
+
+def play_game(game_mode: int, words_count: int) -> None:
+    words_list = WORDS if words_count == len(WORDS) else get_random_words(words_count)
+    print(words_list)
 
 
 def game_flow():
     user_choice, words_count = get_game_mode()
     print(f'user choice: {user_choice}, words count: {words_count}')
+
+    play_game(user_choice, words_count)
